1a31d04be4a37c0998ff01633285f2e53be923c0
Monguersita <anamora.amc@gmail.com>
2025-02-04 17:12:51 +0100

    He comentado la función de ContieneContacto ya que me parecía más cómodo usar el Index (también tenía un fallo) He creado la función Eliminar y creado y probado el btnEliminar

--- GestionContactos/GestionContactos/Form1.cs
+++ GestionContactos/GestionContactos/Form1.cs
@@ -8,7 +8,7 @@ namespace GestionContactos
         {
             InitializeComponent();
         }
-        const int Tam = 10;
+        const int Tam = 5;
         string[] vNombre = new string[Tam];
         string[] vTelefono = new string[Tam];
         void LeerNombre(string[] v, string[] vtlf)
@@ -23,23 +23,63 @@ namespace GestionContactos
         string MostrarVector(string[] v)
         {
             string texto = "";
-            for(int i = 0; i < v.Length; i++)
+            for (int i = 0; i < v.Length; i++)
             {
                 texto += v[i];
                 if (i < v.Length - 1) texto += ", ";
             }
             return texto;
         }
+        int Index(string[] v, string value)
+        {
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] == value)
+                    return i; 
+            }
+            return -1;
+        }
+
+        //bool ContieneContacto(string[] v, string value)
+        //{
+        //    bool result = false;
+        //    for(int i = 0; i <= v.Length; i++)
+        //    {
+        //        if (v[i] == value)
+        //        {
+        //            result = true;
+        //            break;
+        //        }
+        //    }
+        //    return result;
+        //}
+
+        void Eliminar(string nombre)
+        {
+            for(int i = 0; i < vNombre.Length; i++)
+            {
+                if (Index(vNombre, nombre) != -1)
+                {
+                    vNombre[i] = "";
+                    vTelefono[i] = "";
+                }
+            }
+        }
         private void btnAñadir_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Introduzca los usuarios");
             LeerNombre(vNombre, vTelefono);
         }
-
         private void btnMostrar_Click(object sender, EventArgs e)
         {
             MessageBox.Show(MostrarVector(vNombre));
             MessageBox.Show(MostrarVector(vTelefono));
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string nombre = Interaction.InputBox("Introduzca el nombre a eliminar");
+            Eliminar(nombre);
         }
     }
 }
